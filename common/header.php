23d2920771628b8b1a2ec9dfafc3e5dc5fe75c5c
Ritik <scriptdone.software@gmail.com>
2026-01-20 19:00:24 +0530

    changs

---
+++ common/header.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/jquery-ui.css" rel="stylesheet" />
+    <!-- Bootstrap Icon CSS -->
+    <link href="assets/css/bootstrap-icons.css" rel="stylesheet" />
+    <!-- CSS -->
+    <link href="assets/css/animate.min.css" rel="stylesheet" />
+    <!-- FancyBox CSS -->
+    <link href="assets/css/jquery.fancybox.min.css" rel="stylesheet" />
+    <!-- Swiper slider CSS -->
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css" />
+    <!-- Slick slider CSS -->
+    <link rel="stylesheet" href="assets/css/slick.css" />
+    <link rel="stylesheet" href="assets/css/slick-theme.css" />
+    <!-- BoxIcon  CSS -->
+    <link href="assets/css/boxicons.min.css" rel="stylesheet" />
+    <!--  Style CSS  -->
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <!-- Title -->
+    <title>Software Development agency | Scriptdone</title>
+    <link
+      rel="icon"
+      href="./assets//img/custom/scriptdone-fav.png"
+      type="image/png"
+      sizes="20x20"
+    />
+  </head>
+
+  <body>
+    <div class="tt-style-switch index-dark">
+      <i class="bi bi-brightness-low-fill"></i>
+    </div>
+    <!-- scroll top -->
+    <div class="circle-container">
+      <svg
+        class="circle-progress svg-content"
+        width="100%"
+        height="100%"
+        viewBox="-1 -1 102 102"
+      >
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+      </svg>
+    </div>
+    <!-- Start Sidebar section -->
+    
+    <!-- End Sidebar section -->
+
+    <!-- Start header section -->
+    <header class="header-area style-1">
+      <div
+        class="container d-flex flex-nowrap align-items-center justify-content-between"
+      >
+        <div class="company-logo">
+          <a href="index.html" class="logo-dark"
+            ><img alt="image" class="img-fluid" src="assets/scriptdone.png"
+          /></a>
+          <a href="index.html" class="logo-light"
+            ><img alt="image" class="img-fluid" src="assets/scriptdone.png"
+          /></a>
+        </div>
+        <div class="main-menu d-lg-flex d-none">
+          <ul class="menu-list">
+            <li>
+              <a href="about.php" class="drop-down">About</a>
+            </li>
+
+               <li>
+              <a href="service.php" class="drop-down">Services</a>
+            </li>
+
+            <!-- <li class="menu-item-has-children">
+              <a href="service.php" class="drop-down">Services</a
+              ><i class="bi bi-plus dropdown-icon"></i>
+              <ul class="sub-menu">
+                <li><a href="service.html">Service style 01</a></li>
+                <li><a href="service2.html">Service style 02</a></li>
+                <li><a href="service3.html">Service style 03</a></li>
+                <li><a href="service4.html">Service style 04</a></li>
+                <li>
+                  <a href="service-details.html">Service Details</a>
+                </li>
+              </ul>
+            </li> -->
+
+            <li>
+              <a href="portfolio.php" class="drop-down">Protfolio</a>
+            </li>
+
+            <li>
+              <a href="contact.html" class="drop-down">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div class="nav-right d-flex jsutify-content-end align-items-center">
+          <div class="right-sidebar-and-hotline-area">
+            <a
+              href="contact.html"
+              class="primary-btn1"
+              data-text="Free Consultation"
+              ><span>Free Consultation</span></a
+            >
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- End header section -->
